4d457a222481bb1965ef8a23c6eb8e8a1532856b
simohammadi <sina.mohammadi@teliacompany.com>
2020-11-28 23:16:01 +0100

    Added prettier

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import SimpleCard from "./components/card";
 
 function App() {
--- src/components/card.js
+++ src/components/card.js
@@ -1,18 +1,14 @@
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
-import React from "react";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import React from 'react';
 
 function SimpleCard() {
   return (
     <Card>
-      <Card.Header style={{ color: "black" }}>Featured</Card.Header>
+      <Card.Header style={{ color: 'black' }}>Featured</Card.Header>
       <Card.Body>
-        <Card.Title style={{ color: "black" }}>
-          Put the title to twitter thingy here
-        </Card.Title>
-        <Card.Text style={{ color: "black" }}>
-          Put twitter thingy here
-        </Card.Text>
+        <Card.Title style={{ color: 'black' }}>Put the title to twitter thingy here</Card.Title>
+        <Card.Text style={{ color: 'black' }}>Put twitter thingy here</Card.Text>
         <Button variant="primary">Go to top</Button>
       </Card.Body>
     </Card>
