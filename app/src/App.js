565df66f3669b842a9e87f6a3ed5d549c84a1701
simohammadi <sina.mohammadi@teliacompany.com>
2020-12-16 20:40:12 +0100

    New fotter

--- app/src/App.js
+++ app/src/App.js
@@ -1,20 +1,15 @@
 import React, { useState } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import SimpleCard from './components/card';
 import InfinityScroll from './components/infinity-scroll';
+import Fotter from './components/fotter';
 
 function App() {
   return (
     <div className="App">
-      <link
-        rel="stylesheet"
-        href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
-        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
-        crossOrigin="anonymous"
-      />
       <header className="App-header">
         <InfinityScroll />
+        <Fotter />
       </header>
     </div>
   );
