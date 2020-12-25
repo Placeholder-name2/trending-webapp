3a324f1143156bf039f51e8c1d9c712c3669a3de
Simon Samaletdin <simon.samaletdin@gmail.com>
2020-12-25 16:12:40 +0100

    minor stuff

--- app/src/components/api/TwitterApi.js
+++ app/src/components/api/TwitterApi.js
@@ -17,43 +17,52 @@ class TwitterApi extends React.Component {
     componentDidMount() {
 
         const proxy = 'https://cors-anywhere.herokuapp.com/'
-        const api = `${proxy}https://api.twitter.com/1.1/trends/place.json?id=1`
+        const api = `${proxy}https://api.twitter.com/1.1/trends/place.json?id=2459115` //id is currently New York
+        this.getTrendingResult(api, proxy);
+    }
+    getTrendingResult(api, proxy) {
         fetch(api, {
             headers: new Headers({
                 'Authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAAFiyKAEAAAAACvhQhi7bqt7Va64ZVz0tjMHzfzw%3DWJXMBvo6MfSjTgjPO0FeIDyc1maDlIIVkjZ66HIh1AoTjDpv8D'
-                //Do we need origin or x-requested-with headers?
             })
         })
             .then(res => res.json())
             .then(result => {
-                console.log(result)
-                return result[0].trends
+                console.log(result);
+                //here we get 50 trending hashtags, need to create cards for each
+                this.setState({
+                    featuringHashtag: result[0].trends[0].name
+                });
+                return result[0].trends;
             })
-            .then(trends =>
-                fetch(`${proxy}https://api.twitter.com/1.1/search/tweets.json?q=` + trends[0].query, {
-                    headers: new Headers({
-                        'Authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAAFiyKAEAAAAACvhQhi7bqt7Va64ZVz0tjMHzfzw%3DWJXMBvo6MfSjTgjPO0FeIDyc1maDlIIVkjZ66HIh1AoTjDpv8D'
-                    })
-                })
-                    .then(searchRes => searchRes.json())
-                    .then(
-                        (searchResult) => {
-                            console.log(searchResult)
-                            this.setState({
-                                isloaded: true,
-                                twitterId: searchResult.statuses[0].id_str,
-                                featuringHashtag: trends.name
-                            })
-                        },
-                        (error) => {
-                            this.setState({
-                                isLoaded: true,
-                                error
-                            });
-                        }
-                    )
+            .then(trends => this.getSearchResult(proxy, trends)
             );
     }
+
+    getSearchResult(proxy, trends) {
+        return fetch(`${proxy}https://api.twitter.com/1.1/search/tweets.json?q=` + trends[10].query, {
+            headers: new Headers({
+                'Authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAAFiyKAEAAAAACvhQhi7bqt7Va64ZVz0tjMHzfzw%3DWJXMBvo6MfSjTgjPO0FeIDyc1maDlIIVkjZ66HIh1AoTjDpv8D'
+            })
+        })
+            .then(searchRes => searchRes.json())
+            .then(
+                (searchResult) => {
+                    console.log(searchResult);
+                    this.setState({
+                        isLoaded: true,
+                        twitterId: searchResult.statuses[0].id_str,
+                    });
+                },
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            );
+    }
+
     render() {
         const { error, isLoaded, twitterId, featuringHashtag } = this.state;
         if (error) {
@@ -62,14 +71,14 @@ class TwitterApi extends React.Component {
             console.log(twitterId, isLoaded, featuringHashtag)
             return (
                 <ul>
-                    <div>Trending right now on Twitter: {featuringHashtag}</div>
-                    <Tweet tweetId={twitterId}></Tweet>
+                    Loading...
                 </ul>
             );
         } else {
             console.log(twitterId)
             return (
                 <ul>
+                    <div>Trending right now on Twitter: {featuringHashtag}</div>
                     <Tweet tweetId={twitterId}></Tweet>
                 </ul>
             );
--- app/src/components/card.js
+++ app/src/components/card.js
@@ -14,6 +14,7 @@ function SimpleCard(item) {
         <Card.Title style={{ color: 'black' }}>Put the title to twitter thingy here</Card.Title>
         <TwitterApi></TwitterApi>
         {/*<Tweet tweetId="967824267948773377" options={{ theme: "dark" }}></Tweet>*/}
+        {/*to use Tweet element, need to run 'npm install --save react-twitter-widgets' */}
       </Card.Body>
     </Card>
   );
