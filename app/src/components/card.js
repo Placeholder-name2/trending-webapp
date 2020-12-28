fe5bf948fd67a0f5045fcc1ba4218c4c1689e418
Filip <fn@pentech.co>
2020-12-28 13:56:45 +0100

    add spotify iframe -hard coded

--- app/src/components/card.js
+++ app/src/components/card.js
@@ -2,21 +2,37 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import React from 'react';
 import TwitterApi from './api/TwitterApi';
-import { Tweet } from '../../node_modules/react-twitter-widgets'
-
+import { Tweet } from '../../node_modules/react-twitter-widgets';
 
 function SimpleCard(item) {
   return (
-    <Card>
-      {console.log(item.name)}
-      <Card.Header style={{ color: 'black' }}>Featured</Card.Header>
-      <Card.Body>
-        <Card.Title style={{ color: 'black' }}>Put the title to twitter thingy here</Card.Title>
-        <TwitterApi></TwitterApi>
-        {/*<Tweet tweetId="967824267948773377" options={{ theme: "dark" }}></Tweet>*/}
-        {/*to use Tweet element, need to run 'npm install --save react-twitter-widgets' */}
-      </Card.Body>
-    </Card>
+    <div>
+      <Card>
+        {' '}
+        {console.log(item.name)} <Card.Header style={{ color: 'black' }}> Featured </Card.Header>{' '}
+        <Card.Body>
+          <Card.Title style={{ color: 'black' }}> Put the title to twitter thingy here </Card.Title>{' '}
+          <TwitterApi> </TwitterApi> {/*<Tweet tweetId="967824267948773377" options={{ theme: "dark" }}></Tweet>*/}{' '}
+          {/*to use Tweet element, need to run 'npm install --save react-twitter-widgets' */}{' '}
+        </Card.Body>{' '}
+      </Card>
+      <Card>
+        {' '}
+        {console.log(item.name)} <Card.Header style={{ color: 'black' }}> Featured </Card.Header>{' '}
+        <Card.Body>
+          <Card.Title style={{ color: 'black' }}> Put the title to Spotify thingy here </Card.Title>{' '}
+          <iframe
+            title="spotify-test"
+            src="https://open.spotify.com/embed/album/1DFixLWuPkv3KT3TnV35m3"
+            width="300"
+            height="380"
+            frameborder="0"
+            allowtransparency="true"
+            allow="encrypted-media"
+          ></iframe>{' '}
+        </Card.Body>{' '}
+      </Card>{' '}
+    </div>
   );
 }
 
