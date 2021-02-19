13a4226db83937c6a31f65b55ee003c81680fdeb
Simon Samaletdin <simon.samaletdin@gmail.com>
2021-02-19 15:19:19 +0100

    we can fetch data from db but need to figure out how to best parse it to populate cards

--- app/src/dynamodb/database.js
+++ app/src/dynamodb/database.js
@@ -6,9 +6,11 @@ var AWS = require('aws-sdk');
 AWS.config.region = 'eu-north-1'; // Region
 AWS.config.credentials = new AWS.CognitoIdentityCredentials({
   IdentityPoolId: 'eu-north-1:fe098a1d-2503-4ea1-9ded-68da1fede9e8',
+  // RoleArn: 'arn:aws:iam::990232125707:role/Cognito_trending_webappUnauth_Role',
+  // AccountId: '990232125707', // your AWS account ID
 });
 
-AWS.config.update({ region: process.env.REGION });
+//AWS.config.update({ region: process.env.REGION });
 
 var ddb = new AWS.DynamoDB.DocumentClient();//({ apiVersion: '2012-08-10' });
 
@@ -23,9 +25,10 @@ function getDatabaseItems(tableName) {
     if (err) {
       console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
     } else {
-      const databaseItems = JSON.stringify(data, null, 2);
+      console.log("Successfully got JSON from db: \n", JSON.stringify(data, null, 2));
+      const databaseItems = JSON.stringify(data);
       console.log("GetItem succeeded:", databaseItems);
-      return databaseItems
+      return JSON.parse(databaseItems)
     }
   });
 };
